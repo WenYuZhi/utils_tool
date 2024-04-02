ce12947355b102402c2bc382c2236f4be21933e6
王源 <wangyuan@sribd.cn@pc-02-2021-0158-01.sribdl.net>
2024-04-02 14:45:40 +0800

    modified cplex

--- cplex_demo/build/CMakeFiles/cplex_api.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for cplex_api.
-# This may be replaced when dependencies are built.
--- cplex_demo/main.cpp
+++ cplex_demo/main.cpp
@@ -58,7 +58,7 @@ int main(int argc, const char ** argv)
     int ccnt = 0; // the number of new cols
     int rcnt = 1; // the nuboer of new rows
     int nzcnt = 3; // the number of nonzeros constraints coefficients
-    double rhs = 6;
+    double rhs = 20;
     std::vector<int> rmatbeg = {0};
     std::vector<int> rmatind = {0, 1, 3};
     std::vector<double> rmatval = {2, 10, 4};
@@ -70,6 +70,7 @@ int main(int argc, const char ** argv)
     rmatind = {0, 4};
     rmatval = {1, 2};
     nzcnt = 2;
+    rhs = 5;
     status = CPXaddrows(env, lp, ccnt, rcnt, nzcnt, &rhs, "G", rmatbeg.data(), rmatind.data(), rmatval.data(), NULL, NULL);
     
     status = CPXwriteprob(env, lp, "test.lp", NULL);
@@ -77,6 +78,16 @@ int main(int argc, const char ** argv)
         std::cout << "Write Model Error" << std::endl;
     
     status = CPXdelrows(env, lp, 1, 2); // del constraints 1
+    
+    numCols = CPXgetnumcols(env, lp);
+    int numRows = CPXgetnumrows(env, lp);
+    int numNonZeros = CPXgetnumnz(env, lp);
+
+    std::vector<int> delstat(numRows, 0);
+    status = CPXdelsetrows(env, lp, delstat.data());
+
+    delstat = std::vector<int>(numCols, 0); // An array specifying the columns to be deleted.
+    status = CPXdelsetcols(env, lp , delstat.data());  // if delstat[i] = 1 delete column i
 
     // set params
     status = CPXsetdblparam(env, CPX_PARAM_TILIM, 7200);
@@ -93,8 +104,55 @@ int main(int argc, const char ** argv)
 		status = CPXgetobjval(env, lp, &objValue);
 	}
 
+    // get problem data
+    std::vector<char> varTypes(numCols);
+    status = CPXgetctype(env, lp, varTypes.data(), 0, numCols - 1);
+     
+    lb = std::vector<double>(numCols, 0.0);
+    status = CPXgetlb(env, lp, lb.data(), 0, numCols - 1);
+
+    ub = std::vector<double>(numCols, 0.0);
+    status = CPXgetub(env, lp, ub.data(), 0, numCols - 1);
+
+    int objSense = CPXgetobjsen(env, lp); // -1 means max, 1 means min;
+    // status = CPXchgobjsen(env, lp, 1);  change objective sense
+
+    std::vector<double> objCoeffs = std::vector<double>(numCols);
+    status = CPXXgetobj(env, lp, objCoeffs.data(), 0, numCols - 1);
+
+    std::vector<char> rowtypes = std::vector<char>(numRows);
+    status = CPXgetsense(env, lp, rowtypes.data(), 0, numRows - 1);
+
+    std::vector<double> rhs_get = std::vector<double>(numRows);
+    status = CPXgetrhs(env, lp, rhs_get.data(), 0, numRows - 1);
+
+    std::vector<double> rhsrange = std::vector<double>(numRows);
+    status = CPXgetrngval(env, lp, rhsrange.data(), 0, numRows - 1);
+
+    double objOffset = 0;
+    status = CPXgetobjoffset(env, lp, &objOffset);
+
+    std::vector<int> rowStart = std::vector<int>(numRows);
+    std::vector<int> colIdxs = std::vector<int>(numNonZeros);
+    std::vector<double> colCoeffs = std::vector<double>(numNonZeros);
+    
+    int count = 0, surplus_p;
+    status = CPXgetrows(env, lp, &count, rowStart.data(), colIdxs.data(), colCoeffs.data(),
+    numNonZeros, &surplus_p, 0, numRows - 1);
+    
+    std::vector<int> cmatbeg = std::vector<int>(numCols);
+	std::vector<int> cmatind = std::vector<int>(numNonZeros);
+	std::vector<double> cmatval = std::vector<double>(numNonZeros);
+    status = CPXgetcols(env, lp, &count, cmatbeg.data(), cmatind.data(), cmatval.data(), 
+    numNonZeros, &surplus_p, 0, numCols - 1);
+
+
+    status = CPXchgprobtype(env, lp, CPXPROB_LP); // problem type: CPXPROB_MILP, CPXPROB_LP
+    status = CPXlpopt(env, lp);
+
+    status = CPXreadcopyprob(env, lp, "test.lp", NULL);
+
     CPXfreeprob(env, &lp);
     CPXcloseCPLEX(&env);
-
     return 0;
 }
